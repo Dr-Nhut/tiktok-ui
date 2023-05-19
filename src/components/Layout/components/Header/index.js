6a483a2ddd03b5ab4665419b811483b798c94a29
Dr-Nhut <leminhnhut1612@gmail.com>
2023-05-19 15:40:40 +0700

    Dựng khung DefaultLayout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles); // Giup viet ten class duoi dang: post-Item, neu khong se mac dinh la camelCase;
 function Header() {
-    return (<h1>Header</h1>);
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('content')}></div>
+        </header>
+    );
 }
 
 export default Header;
